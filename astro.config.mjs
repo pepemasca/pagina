f7dc5cf38c7ba29ed20c700c1d5bf94a0468e6be
pepemasca <javiermascga2@gmail.com>
2024-12-04 20:16:21 +0100

    opengraph2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://cryptopedia-com.web.app",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   server: {
     host: '0.0.0.0',
